ca0c58793592ce6b6db93a157fef31637fb1f070
Jash-007 <76209078+Jash-007@users.noreply.github.com>
2024-01-24 18:22:11 +0530

    Update code.js

--- Transform Over Array/code.js
+++ Transform Over Array/code.js
@@ -4,21 +4,6 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let res=[]
-    let i=0
-    while(i<=arr.length-1){
-        res[i]=fn(arr[i],i);
-        i++;
-    }
-    return res;
-};
-Updated code with single array
-/**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
- */
-var map = function(arr, fn) {
    
     let i=0
     while(i<=arr.length-1){
