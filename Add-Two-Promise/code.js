99ad19d6449c6113f49f7c6a9a3ebea6be16b793
Jash-007 <76209078+Jash-007@users.noreply.github.com>
2024-01-24 12:40:20 +0530

    Update code.js

--- Add-Two-Promise/code.js
+++ Add-Two-Promise/code.js
@@ -3,10 +3,9 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function(promise1, promise2) {
-    const v1= await promise1;
-    const v2=await promise2;
-    return v1+v2;
+var addTwoPromises = async function (promise1, promise2) {
+    const [v1, v2] = await Promise.all([promise1, promise2]);
+    return v1 + v2;
 };
 
 /**
