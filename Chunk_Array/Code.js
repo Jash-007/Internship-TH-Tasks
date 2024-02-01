71a076f6a13d3fbd4fcd7bcd972f2e5b4ee56736
Jash-007 <76209078+Jash-007@users.noreply.github.com>
2024-02-01 12:31:48 +0530

    Update Code.js

--- Chunk_Array/Code.js
+++ Chunk_Array/Code.js
@@ -4,9 +4,11 @@
  * @return {Array}
  */
 var chunk = function (arr, size) {
-    const a = []
-    for (var i = 0; i <= arr.length - 1; i = i + size) {
-        a.push(arr.slice(i, i + size));
-    }
-    return a;
+    return arr.reduce((acc, cur) => {
+        if (acc.length == 0 || acc.at(-1).length == size) {
+            acc.push([]);
+        }
+        acc.at(-1).push(cur);
+        return acc;
+    }, []);
 };
