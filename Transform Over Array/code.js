5e2c41ffd398a8c0bf26df70611b26898f9b605d
Jash-007 <76209078+Jash-007@users.noreply.github.com>
2024-02-01 12:30:34 +0530

    Update code.js

--- Transform Over Array/code.js
+++ Transform Over Array/code.js
@@ -3,12 +3,10 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-   
-    let i=0
-    while(i<=arr.length-1){
-        arr[i]=fn(arr[i],i);
-        i++;
+var map = function (arr, fn) {
+    let result = arr.slice();
+    for (let i = 0; i < result.length; i++) {
+        result[i] = fn(result[i], i);
     }
-    return arr;
+    return result;
 };
